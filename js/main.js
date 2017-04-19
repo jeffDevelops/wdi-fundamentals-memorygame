dc7d7275e65f7fb868335f41c5c33a93badbdf82
jeffDevelops <jefferydreynolds@gmail.com>
2017-04-19 12:50:16 -0600

    refactored js with some functions to simulate flipping cards and checking for matches; for now, only works with two selections of cards and user cannot select them via DOM Manipulation.

--- js/main.js
+++ js/main.js
@@ -1,18 +1,23 @@
 var cards = ["queen", "queen", "king", "king"];
 var cardsInPlay = [];
 
-var cardOne = cards[0];
-cardsInPlay.push(cardOne);
-
-var cardTwo = cards[1];
-cardsInPlay.push(cardTwo);
-
-console.log(cardsInPlay);
-
-if(cardsInPlay.length === 2) {
-	if(cardsInPlay[0] === cardsInPlay[1]) {
-		alert("You found a match!");
-	} else {
-		alert("Sorry, try again!");
+var checkForMatch = function() { //Once there are two cards in the cardsInPlay array, we can see whether they're the same suit
+	if(cardsInPlay.length === 2) {
+		if(cardsInPlay[0] === cardsInPlay[1]) {
+			alert("You found a match!");
+		} else {
+			alert("Sorry, try again!");
+		}
 	}
 }
+
+var flipCard = function(cardId) { //Simulates flipping one card by storing the chosen card into the cardsInPlay array
+	console.log("User flipped a " + cards[cardId] + " card.");
+	cardsInPlay.push(cards[cardId]);
+	checkForMatch();
+}
+
+flipCard(0);
+flipCard(2);
+
+
