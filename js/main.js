31a35e60bc2a834a1683d043abf231e64574e980
jeffDevelops <jefferydreynolds@gmail.com>
2017-06-08 14:37:11 -0600

    created game logic and added animations

--- js/main.js
+++ js/main.js
@@ -20,27 +20,98 @@ var cards = [
 		cardImage: "images/king-of-diamonds.png"
 	}
 ];
+var cardElements = [];
 var cardsInPlay = [];
+var isMatch = false;
+
+var modal = document.createElement('div');
+var msg = document.createElement('h2');
+
+modal.setAttribute('class', 'match-modal');
+msg.setAttribute('class', 'match-message');
+
+
+var clearModal = function() {
+	modal.classList.add('modal-animation');
+	modal.classList.remove('match-modal');
+	msg.classList.add('msg-animation');
+	msg.classList.remove('match-message');	
+}
 
 var checkForMatch = function() { //Once there are two cards in the cardsInPlay array, we can see whether they're the same suit
-	if(cardsInPlay.length === 2) {
-		if(cardsInPlay[0] === cardsInPlay[1]) {
-			alert("You found a match!");
+		if(cardsInPlay[0].rank === cardsInPlay[1].rank) {
+			//Match message
+			msg.textContent = 'You found a match!';
+			//Assemble the modal
+			document.getElementById('game-board').appendChild(modal);
+			document.querySelector('.match-modal').appendChild(msg);
+			//Clear the modal
+			setTimeout(clearModal, 250);
+			//Reset cardsInPlay Array
+			cardsInPlay = [];
 		} else {
-			alert("Sorry, try again!");
+			//Not a match message
+			msg.textContent = 'Not a match.';
+			//Assemble the modal
+			document.getElementById('game-board').appendChild(modal);
+			document.querySelector('.match-modal').appendChild(msg);
+			//Clear the modal
+			setTimeout(clearModal, 250);
+			//Flip the cards back
+			window.setTimeout(function() {
+				for(i = 0; i < cardElements.length; i++) {
+					cardElements[i].removeAttribute('src', cards[cardId].cardImage)
+					cardElements[i].setAttribute('src', 'images/back.png');
+				}
+			}, 1500);
+
+			//Reset cardsInPlay array
+			cardsInPlay = [];
+
+			cardElements.forEach(function(element) {
+				element.addEventListener('click', flipCard);
+			});
+		}
+}
+
+var flipCardsBack = function() { //THIS FUNCTION ISN'T RUNNING FOR SOME REASON
+	console.log("flipCardsBack() running");
+
+}
+
+var flipCard = function() { //Simulates flipping one card by storing the chosen card into the cardsInPlay array
+	cardId = this.getAttribute('data-id');
+	this.setAttribute('src', cards[cardId].cardImage);
+	console.log("User flipped a " + cards[cardId].rank + " card.");
+	cardsInPlay.push(cards[cardId]); //push the card the user clicked up to the array
+	if(cardsInPlay.length === 2) { //If there are two cards in the array
+		if(cardsInPlay[0] !== cardsInPlay[1]) { //and as long as the user hasn't clicked the same card twice...
+			checkForMatch();
+		} else { //if they have clicked the same card twice,
+			cardsInPlay = [];//clear the array
+			this.setAttribute('src', 'images/back.png');//flip all cards back
+			msg.textContent = "Hey! Don't peek!";
+			document.getElementById('game-board').appendChild(modal);
+			document.querySelector('.match-modal').appendChild(msg);
+			//Clear the modal
+			setTimeout(clearModal, 250);
 		}
 	}
 }
 
-var flipCard = function(cardId) { //Simulates flipping one card by storing the chosen card into the cardsInPlay array
-	console.log("User flipped a " + cards[cardId].rank + " card.");
-	cardsInPlay.push(cards[cardId].rank);
-	console.log(cards[cardId].cardImage);
-	console.log("of " + cards[cardId].suit);
-	checkForMatch();
+var createBoard = function() {
+	for(var i = 0; i < cards.length; i++) {
+		cardElement = document.createElement('img');
+		cardElement.setAttribute('src', 'images/back.png');
+		cardElement.setAttribute('data-id', i);
+		cardElement.addEventListener('click', flipCard);
+		document.getElementById('game-board').appendChild(cardElement);
+		cardElements.push(cardElement);
+	}
 }
 
-flipCard(0);
-flipCard(2);
+createBoard();
+
+
 
 
